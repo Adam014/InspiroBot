4608e8fde2399df12cf9da8186ba57a981e13d7a
Adam Stádník <adam.stadnik@seznam.cz>
2023-10-23 21:15:03 +0200

    Resolved a bug with login

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -5,44 +5,47 @@ import { connectToDB } from "@utils/database";
 import User from "@models/user";
 
 const handler = NextAuth({
-    providers:[
+    providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            checks: ['none'],
         })
     ],
-    async session({ session }){
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
+    callbacks: {
+        async session({ session }){
+            const sessionUser = await User.findOne({
+                email: session.user.email
+            })
 
-        session.user.id = sessionUser._id.toString();
+            session.user.id = sessionUser._id.toString();
 
-        return session;
-    },
+            return session;
+        },
 
-    // function for creating and sign in user
-    async signIn({ profile }){
-        try {
-            await connectToDB();
-            // check if a user already exists
-            const userExists = await User.findOne({
-                email: profile.email
-            });
-            // if not, create a new user
-            if(!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture,
-                })
+        // function for creating and sign in user
+        async signIn({ profile }){
+            try {
+                await connectToDB();
+                // check if a user already exists
+                const userExists = await User.findOne({
+                    email: profile.email
+                });
+                // if not, create a new user
+                if(!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture,
+                    })
+                }
+                return true;
+            } catch (error) {
+                console.log(error);
+                return false;
             }
-            return true;
-        } catch (error) {
-            console.log(error);
-            return false;
-        }
-    },
+        },
+    }
 })
 
 export { handler as GET, handler as POST };
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
 
             <Link href="/profile">
               <Image 
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className='rounded-full'
@@ -76,7 +76,7 @@ const Navbar = () => {
         {session?.user ? (
             <div className='flex'>
               <Image 
-                  src="/assets/images/logo.svg"
+                  src={session?.user.image}
                   width={37}
                   height={37}
                   className='rounded-full'
