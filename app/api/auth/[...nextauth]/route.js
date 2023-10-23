41e723dbbc8b2bf37d9e533f8e4756f2da1a9dd2
Adam Stádník <adam.stadnik@seznam.cz>
2023-10-23 18:05:08 +0200

    Added a MongoDB connection and created function for sign in and user creation

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
 
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
+
 const handler = NextAuth({
     providers:[
         GoogleProvider({
@@ -12,11 +15,26 @@ const handler = NextAuth({
         
     },
 
+    // function for creating and sign in user
     async signIn({ profile }){
         try {
-            
+            await connectToDB();
+            // check if a user already exists
+            const userExists = await User.findOne({
+                email: profile.email
+            });
+            // if not, create a new user
+            if(!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture,
+                })
+            }
+            return true;
         } catch (error) {
-
+            console.log(error);
+            return false;
         }
     },
 })
